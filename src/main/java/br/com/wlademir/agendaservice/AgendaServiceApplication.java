40faaa42baa536b9ea907498eb0f2d044b6e96af
unknown <grolli502@gmail.com>
2021-10-04 22:53:30 -0300

    comit inicial

---
+++ src/main/java/br/com/wlademir/agendaservice/AgendaServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.wlademir.agendaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wlademir/agendaservice/AgendaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wlademir.agendaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
